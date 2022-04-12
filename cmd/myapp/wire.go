c655e99e9373771d49fd5de1d947da4f1c579ec3
gs fage <879756530@qq.com>
2022-04-12 11:04:57 +0800

    1

---
+++ cmd/myapp/wire.go
@@ -0,0 +1,30 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+package main
+
+import (
+	"github.com/google/wire"
+	"gomicroddd/"
+)
+
+//go:generate wire
+var providerSet = wire.NewSet(
+	// conf.NewViper,
+	// conf.NewAppConfigCfg,
+	// conf.NewLoggerCfg,
+	// conf.NewRedisConfig,
+	// conf.NewMongoConfig,
+	// log.NewLogger,
+	// redis.NewRedis,
+	// mongo.NewMongo,
+	repository.NewRepository,
+	aggregate.NewFactory,
+	service.NewService,
+	adpter.NewSrv,
+)
+
+func NewApp() (*adpter.Server, error) {
+	panic(wire.Build(providerSet))
+}
