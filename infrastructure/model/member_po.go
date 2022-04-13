f74881e48d316b86c3777e877d6cc88dd71e1875
gs fage <879756530@qq.com>
2022-04-13 17:20:21 +0800

    2

--- infrastructure/model/member_po.go
+++ infrastructure/model/member_po.go
@@ -1,23 +1,38 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"microddd/domain/entity"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type User_po struct {
-	ID        uuid.UUID
-	LoginName string
-	Email     string
-	Password  string
+	ID        uuid.UUID `gorm:"primarykey;not null;unqua"`
+	LoginName string    `gorm:"not null; "`
+	Email     string    `gorm:"not null; size:30"`
+	Password  string    `gorm:"not null ;size:50"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type Role_po struct {
-	ID       uuid.UUID
-	RoleName string
-	Remark   string
+	ID        uuid.UUID `gorm:"primarykey;not null;unqua"`
+	RoleName  string    `gorm:"size:20"`
+	Remark    string    `gorm:"size:200"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type UserRole_po struct {
-	ID     uuid.UUID
-	RoleID uuid.UUID
-	UserID uuid.UUID
-	Status int
+	ID        uuid.UUID `gorm:"primarykey;not null;unqua"`
+	RoleID    uuid.UUID `gorm:"not null"`
+	UserID    uuid.UUID `gorm:"not null"`
+	Status    int       `gorm:""`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (ul *UserRole_po) ToDo() *entity.UserRoleEntity {
+	return &entity.UserRoleEntity{}
 }
