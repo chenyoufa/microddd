ad4d4e236fc3d699fe029ae6e908ab00c6a563fd
gs fage <879756530@qq.com>
2022-04-14 16:37:50 +0800

    3

--- application/member_app.go
+++ application/member_app.go
@@ -10,6 +10,11 @@ import (
 
 type MemberApper interface {
 	Get(ctx context.Context, uuid uuid.UUID) (*aggregate.Member_aggre, error)
+	GetList(ctx context.Context, uuid uuid.UUID) ([]*aggregate.Member_aggre, error)
+	Add(ctx context.Context, aggre *aggregate.Member_aggre) (bool, error)
+	Edit(ctx context.Context, aggre *aggregate.Member_aggre) (bool, error)
+	Login(ctx context.Context, usname string, pwd string) (bool, error)
+	Logout(ctx context.Context, usname string, pwd string) (bool, error)
 }
 
 type memberApp struct {
@@ -33,4 +38,27 @@ func (u *memberApp) Get(ctx context.Context, uuid uuid.UUID) (*aggregate.Member_
 		return nil, err
 	}
 	return m, nil
+}
+
+func (u *memberApp) GetList(ctx context.Context, uuid uuid.UUID) ([]*aggregate.Member_aggre, error) {
+	list, err := u.mRepo.GetList(ctx, uuid)
+	return list, err
+}
+func (u *memberApp) Add(ctx context.Context, aggre *aggregate.Member_aggre) (bool, error) {
+
+	isbool, err := u.mRepo.Add(ctx, aggre)
+	return isbool, err
+}
+func (u *memberApp) Edit(ctx context.Context, aggre *aggregate.Member_aggre) (bool, error) {
+
+	isbool, err := u.mRepo.Edit(ctx, aggre)
+	return isbool, err
+}
+func (u *memberApp) Login(ctx context.Context, usname string, pwd string) (bool, error) {
+	isbool, err := u.mRepo.Login(ctx, usname, pwd)
+	return isbool, err
+}
+func (u *memberApp) Logout(ctx context.Context, usname string, pwd string) (bool, error) {
+
+	return true, nil
 }
